91d4584c085758b0e2df63eda0c49853134e1a30
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2025-03-24 14:33:17 +0800

    redesign the comment section and add the comment ability to the section using JS (#2)

--- mejares_jamesmichael/script.js
+++ mejares_jamesmichael/script.js
@@ -1,23 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     const commentName = document.getElementById("comment_name");
     const commentMessage = document.getElementById("comment_message");
     const commentButton = document.querySelector("#comment_button button");
+    const commentContainer = document.querySelector(".comment-container");
 
-    function toggleButtonState() {
-        if (commentName.value.trim() !== "" && 
-            commentMessage.value.trim() !== "") {
-            commentButton.disabled = false;
-            commentButton.style.opacity = "1";
-            commentButton.style.cursor = "pointer";
-        } else {
-            commentButton.disabled = true;
-            commentButton.style.opacity = "0.5";
-            commentButton.style.cursor = "not-allowed";
-        }
-    }
+    const toggleButtonState = () => {
+        const isFormValid = commentName.value.trim() 
+        && commentMessage.value.trim();
+        commentButton.disabled = !isFormValid;
+        commentButton.style.opacity = isFormValid ? "1" : "0.5";
+        commentButton.style.cursor = isFormValid ? "pointer" : "not-allowed";
+    };
 
-    commentName.addEventListener("input", toggleButtonState);
-    commentMessage.addEventListener("input", toggleButtonState);
+    const addComment = () => {
+        const commentDiv = document.createElement("div");
+        commentDiv.classList.add("comment-message");
+        commentDiv.innerHTML = 
+            `${commentMessage.value.trim()}
+            <div class="comment-author">- ${commentName.value.trim()}</div>`;
+        commentContainer.appendChild(commentDiv);
+        commentName.value = "";
+        commentMessage.value = "";
+
+        toggleButtonState();
+    };
+
+    [commentName, commentMessage].forEach(input =>
+        input.addEventListener("input", toggleButtonState)
+    );
+    commentButton.addEventListener("click", addComment);
 
     toggleButtonState();
 });
